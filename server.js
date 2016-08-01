43a77bddb2c31ffd46082c81db6a46e50e77bfe4
Robert Yang <yang.robert.w@gmail.com>
2016-08-01 02:21:29 -0400

    using the example from OpenShift

--- server.js
+++ server.js
@@ -1,4 +1,36 @@
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var WebSocketServer = require('ws').Server
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	  response.write("Welcome to Node.js on OpenShift!\n\n");
+	  response.end("Thanks for visiting us! \n");
+});
+
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wss = new WebSocketServer({
+    server: server,
+    autoAcceptConnections: false
+});
+wss.on('connection', function(ws) {
+  console.log("New connection");
+  ws.on('message', function(message) {
+    ws.send("Received: " + message);
+  });
+  ws.send('Welcome!');
+});
+
+console.log("Listening to " + ipaddress + ":" + port + "...");
+
+/*
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var app = require("express");
 var server = require("http").Server(app);
@@ -14,7 +46,7 @@ io.on("connection", handleClient);
 console.log("listen: "+ipaddress+" "+port);
 server.listen(port, ipaddress);
 
-
+*/
 
 //Openshift variables
 /*
