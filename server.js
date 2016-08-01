8ee2159458db7dfeb77de23f4f53716ac35d532c
Robert Yang <yang.robert.w@gmail.com>
2016-08-01 00:49:08 -0400

    uhh

--- server.js
+++ server.js
@@ -1,159 +1,103 @@
-#!/bin/env node
-//  OpenShift sample Node application
-var express = require('express');
-var fs      = require('fs');
+var net = require('net');
+var sockets = [];
+var port = 8000;
+var guestId = 0;
+
+var server = net.createServer(function(socket) {
+	// Increment
+	guestId++;
+	
+	socket.nickname = "Guest" + guestId;
+	var clientName = socket.nickname;
+
+	sockets.push(socket);
+
+	// Log it to the server output
+	console.log(clientName + ' joined this chat.');
+
+	// Welcome user to the socket
+	socket.write("Welcome to telnet chat!\n");
+
+	// Broadcast to others excluding this socket
+	broadcast(clientName, clientName + ' joined this chat.\n');
 
 
-/**
- *  Define the sample application.
- */
-var SampleApp = function() {
+	// When client sends data
+	socket.on('data', function(data) {
 
-    //  Scope.
-    var self = this;
+		var message = clientName + '> ' + data.toString();
 
+		broadcast(clientName, message);
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
-
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
-    };
+		// Log it to the server output
+		process.stdout.write(message);
+	});
 
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
+	// When client leaves
+	socket.on('end', function() {
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+		var message = clientName + ' left this chat\n';
 
+		// Log it to the server output
+		process.stdout.write(message);
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
+		// Remove client from socket array
+		removeSocket(socket);
+
+		// Notify all clients
+		broadcast(clientName, message);
+	});
 
 
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
+	// When socket gets errors
+	socket.on('error', function(error) {
+
+		console.log('Socket got problems: ', error.message);
+
+	});
+});
 
 
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
+// Broadcast to others, excluding the sender
+function broadcast(from, message) {
 
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
+	// If there are no sockets, then don't broadcast any messages
+	if (sockets.length === 0) {
+		process.stdout.write('Everyone left the chat');
+		return;
+	}
 
+	// If there are clients remaining then broadcast message
+	sockets.forEach(function(socket, index, array){
+		// Dont send any messages to the sender
+		if(socket.nickname === from) return;
+		
+		socket.write(message);
+	
+	});
+	
+};
 
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
+// Remove disconnected client from sockets array
+function removeSocket(socket) {
 
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
+	sockets.splice(sockets.indexOf(socket), 1);
 
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
-
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
-    };
+};
 
 
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express.createServer();
+// Listening for any problems with the server
+server.on('error', function(error) {
 
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-    };
+	console.log("So we got problems!", error.message);
 
+});
 
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
+// Listen for a port to telnet to
+// then in the terminal just run 'telnet localhost [port]'
+server.listen(port, function() {
 
-        // Create the express server and routes.
-        self.initializeServer();
-    };
+	console.log("Server listening at http://localhost:" + port);
 
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
-        });
-    };
-
-};   /*  Sample Application.  */
-
-
-
-/**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
-zapp.initialize();
-zapp.start();
-
+});
